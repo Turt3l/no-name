cfed0b4809e02b19486a2eccffcdee27868e0565
Turt3l <mikelisekmanis13@gmail.com>
2023-08-30 20:35:50 +0300

    initial commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,24 @@
+export default function Header() {
+    return(
+        <div className="Header">
+            <div className="HeaderContainer">
+                <div className="HeaderMainName">
+                    Lorem Ipsum
+                </div>
+                <div className="HeaderLinkContainer">
+                    <div className="HeaderLinks">
+                        <div className="LogoContainer">
+                            
+                        </div>
+                        <div className="LogoContainer">
+                            
+                        </div>
+                        <div className="LogoContainer">
+                            
+                        </div>    
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
