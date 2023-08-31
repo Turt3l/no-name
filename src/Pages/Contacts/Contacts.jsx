87f644f564cfae51abfcf2237fb1569c9537b8fe
KanKan-coding <markusskancans05@gmail.com>
2023-08-31 10:08:22 +0300

    started work on the contacts page

---
+++ src/Pages/Contacts/Contacts.jsx
@@ -0,0 +1,12 @@
+import Header from "../../Components/Header"
+
+export default function ContactsPage() {
+    return (
+        <div className="ContactsContainer">
+            <Header />
+            <div className="background">
+
+            </div>
+        </div>
+    )
+}
