5a67efca8aa400ffda017be6c421255f9a3c8da4
KanKan-coding <markusskancans05@gmail.com>
2023-09-16 11:56:40 +0300

    added logos to header and added finishing touches to the contacts page

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,7 @@
 import { Link } from "react-router-dom";
+import instagram from "../Media/Icons Folder/instagram.svg";
+import facebook from "../Media/Icons Folder/facebook.svg";
+import twitter from "../Media/Icons Folder/twitter.svg"
 export default function Header() {
 
     return(
@@ -10,13 +13,13 @@ export default function Header() {
                 <div className="HeaderLinkContainer">
                     <div className="HeaderLinks">
                         <div className="LogoContainer">
-                            
+                            <img src={instagram} alt="" id="socialLogo"/>
                         </div>
                         <div className="LogoContainer">
-                            
+                            <img src={facebook} alt="" id="socialLogo"/>
                         </div>
                         <div className="LogoContainer">
-                            
+                            <img src={twitter} alt="" id="socialLogo"/>
                         </div>    
                     </div>
                 </div>
