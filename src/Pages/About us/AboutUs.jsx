7616a0f647a3de85d950938b82cd52b1d8043047
KanKan-coding <markusskancans05@gmail.com>
2023-09-16 19:37:20 +0300

    started work on the about us page

---
+++ src/Pages/About us/AboutUs.jsx
@@ -0,0 +1,12 @@
+import "../../style.css"
+import background from "../../Media/background.png"
+
+export default function AboutUsPage() {
+    return (
+        <div className="AboutContainer">
+            <div className="background">
+                <img src={background} alt="" />
+            </div>
+        </div>
+    )
+}
