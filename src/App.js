ae771e4e8f5f99a6628bf1fac04df81a37dd2b35
KanKan-coding <markusskancans05@gmail.com>
2023-09-16 19:38:51 +0300

    synced everything

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Header from "./Components/Header";
 import ContactsPage from "./Pages/Contacts/Contacts";
-import LandingPage from "./Pages/Landing/Landing";
 import {
   BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
-import Services from "./Pages/Services/Services";
 function App() {
   return (
     <div className="App">
@@ -14,7 +12,6 @@ function App() {
       <Header />
         <Routes>
           <Route path='/' element={<LandingPage/>}/>
-          <Route path="/contacts" element={<ContactsPage/>} />
           <Route path="/services" element={<Services/>}/>
         </Routes>
       </BrowserRouter>
