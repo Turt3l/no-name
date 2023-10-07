e55e4d1351d41891d0f7889b8ad0be4e73fabbee
KanKan-coding <markusskancans05@gmail.com>
2023-10-07 16:15:08 +0300

    sync

--- src/Pages/About us/AboutUs.jsx
+++ src/Pages/About us/AboutUs.jsx
@@ -11,15 +11,16 @@ export default function AboutUsPage() {
                     <h1 className="mainText">About Us</h1>
                     <h2 className="secText">A short description of what we do</h2>
                 </div>
-            </div>
-            <div className="aboutUsBottomDiv">                
-                <div className="imageLeftSide">
-                    (Bilde)
+                <div className="aboutUsBottomDiv">                
+                    <div className="imageLeftSide">
+                        (Bilde)
+                    </div>
+                    <div className="textRightSide">
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.</p>
+                    </div>
                 </div>
-                <div className="textRightSide">
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.</p>
-                </div>
             </div>
+            
         </div>
     )
 }
