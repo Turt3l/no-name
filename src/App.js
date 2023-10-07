26947e521b7b2cfaed57641dba43aad71c09066e
Turt3l <mikelisekmanis13@gmail.com>
2023-10-07 16:11:15 +0300

    background changes

--- src/App.js
+++ src/App.js
@@ -9,19 +9,11 @@ import {
 import Services from "./Pages/Services/Services";
 import AboutUsPage from "./Pages/About us/AboutUs";
 import Footer from "./Components/Footer";
-import { useEffect, useState } from "react";
-import Loading from "./Pages/Loading/Loading";
 
 function App() {
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 3000)
-  }, [])
   return (
     <div className="App">
-      {loading && <Loading />}
-      {<BrowserRouter>
+      <BrowserRouter>
       <Header />
         <Routes>
           <Route path='/' element={<LandingPage/>}/>
@@ -30,7 +22,7 @@ function App() {
           <Route path="/aboutus" element={<AboutUsPage/>}/>
         </Routes>
         <Footer/>
-      </BrowserRouter>}
+      </BrowserRouter>
     </div>
   );
 }
